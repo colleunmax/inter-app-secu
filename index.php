3da6ab5b395e689c9b2227fdfb6a1f7590b91873
KoruJuriSan <mr.collejoulian@gmail.com>
2024-12-20 15:47:22 +0100

    fix: bug where the / didn't work as the root of the projet on windows

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (empty($_SESSION['csrf_token'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/public/styles.css">
+    <link rel="stylesheet" href="public/styles.css">
     <title>SS | Connexion</title>
 </head>
 <body>
