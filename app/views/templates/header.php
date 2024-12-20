6775c45b315734b22c16f230f5fac7a601e93196
KoruJuriSan <mr.collejoulian@gmail.com>
2024-12-20 02:30:46 +0100

    ci:  rework of the header html base

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -1,3 +1,23 @@
 <header>
-    <h1> Application de la sécurité</h1>
+    <div>
+        <a href="">
+            <img src="/public/assets/logo.svg" alt="In a pixelised font: SS Smartcity Security">
+        </a>
+        <ul>
+            <li>
+                <form method="GET" action="index.php" style="margin-top: 20px;">
+                    <input type="hidden" name="controller" value="dashboard">
+                    <input type="hidden" name="action" value="index">
+                    <button type="submit">dashboard</button>
+                </form>
+            </li>
+            <li>
+                <form method="GET" action="index.php" style="margin-top: 20px;">
+                    <input type="hidden" name="controller" value="alert">
+                    <input type="hidden" name="action" value="index">
+                    <button type="submit">enregistrements</button>
+                </form>
+            </li>
+        </ul>
+    </div>
 </header>
