ee20cc9cef528fdc587d797d68eafd9158c1c0d2
KoruJuriSan <mr.collejoulian@gmail.com>
2024-12-20 05:43:49 +0100

    feat: updating database Utils to be used later down the line.

--- core/database.php
+++ core/database.php
@@ -1,24 +1,68 @@
 <?php
 /**
- * database.php
- *
- * Ce fichier contient les connexions à la base de données.
- * Deux connexions principales sont configurées : une pour la sécurité et une pour le système smartcity.
- * Les fonctions incluent la gestion des exceptions liées à la base de données.
- * C'est un fichier central pour toutes les opérations de base de données.
+ * Class Database
+ * 
+ * Utilitaire de connexion à différentes bases de données.
  */
-
-require_once '../config.php';
-
 class Database {
-    public static function connect() {
-        try {
-            $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $pdo;
-        } catch (PDOException $e) {
-            die("Erreur : " . $e->getMessage());
-        }
+    
+    /**
+     * Crée une connexion PDO à une base de données spécifique.
+     *
+     * @param string $host L'adresse du serveur de base de données.
+     * @param string $dbName Le nom de la base de données.
+     * @param string $user Le nom d'utilisateur pour la connexion.
+     * @param string $passwd Le mot de passe pour la connexion.
+     * 
+     * @return PDO L'instance PDO pour la connexion à la base de données.
+     */
+    protected static function connect(string $host, string $dbName, string $user, string $passwd) {
+        $pdo = new PDO(
+            "mysql:host=".$host.";dbname=".$dbName,
+            $user,
+            $passwd,
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+        $pdo->exec("SET NAMES utf8;");
+        return $pdo;
+    }
+
+    /**
+     * Retourne une instance PDO connectée à la base de données sécurisée.
+     *
+     * @return PDO L'instance PDO pour la connexion à la base de données sécurisée.
+     */
+    public static function getSecurityPDO(): PDO {
+        $host = $_ENV["LOCAL_HOST"];
+        $dbName = $_ENV["LOCAL_DBNAME"];
+        $user = $_ENV["LOCAL_USERNAME"];
+        $password = $_ENV["LOCAL_PASSWORD"];
+        return self::connect($host, $dbName, $user, $password);
+    }
+
+    /**
+     * Retourne une instance PDO connectée à la base de données slave de Smartcity.
+     *
+     * @return PDO L'instance PDO pour la connexion à la base de données slave de Smartcity.
+     */
+    public static function getSlaveSmartcityPDO(): PDO {
+        $host = $_ENV["SLAVE_HOST"];
+        $dbName = $_ENV["SLAVE_DBNAME"];
+        $user = $_ENV["SLAVE_USERNAME"];
+        $password = $_ENV["SLAVE_PASSWORD"];
+        return self::connect($host, $dbName, $user, $password);
+    }
+
+    /**
+     * Retourne une instance PDO connectée à la base de données master de Smartcity.
+     *
+     * @return PDO L'instance PDO pour la connexion à la base de données master de Smartcity.
+     */
+    public static function getMasterSmartcityPDO(): PDO {
+        $host = $_ENV["MASTER_HOST"];
+        $dbName = $_ENV["MASTER_DBNAME"];
+        $user = $_ENV["MASTER_USERNAME"];
+        $password = $_ENV["MASTER_PASSWORD"];
+        return self::connect($host, $dbName, $user, $password);
     }
 }
-?>
